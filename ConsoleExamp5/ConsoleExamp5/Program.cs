942770a8f393dead8ba847f573e33960ee8a48bb
ปิยชัย กิจกาญจน์ <105155094+Piyachai3671@users.noreply.github.com>
2026-02-13 22:18:15 +0700

    ข้อ 5. โปรแกรมเรียงลำดับตัวเลข

---
+++ ConsoleExamp5/ConsoleExamp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExamp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+          
+            while (true)
+            {
+                Console.Write("Please Input Positive Integer Value: ");
+                if (int.TryParse(Console.ReadLine(), out input) && input > 0)
+                {
+                    break;
+                }
+            
+                Console.WriteLine("Invalid input. Try again.\n");
+            }
+          int result= SetDataInt(input);
+            Console.WriteLine("Result is : " + result);
+            Console.ReadLine();
+        }
+        static int SetDataInt(int input)
+        {
+            var arr = input.ToString().ToArray();
+            var orderDes = arr.OrderByDescending(e => e).ToArray();
+            return Convert.ToInt32(string.Join("", orderDes));
+             
+        }
+    }
+}
